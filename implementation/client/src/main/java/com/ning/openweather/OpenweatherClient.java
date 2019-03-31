49c9202feda5191b41ce8a3a37d5d790014bc093
Ning Dai <daining1298@hotmail.com>
2019-03-31 08:07:27 +0300

    add weather client

---
+++ implementation/client/src/main/java/com/ning/openweather/OpenweatherClient.java
@@ -0,0 +1,11 @@
+package com.ning.openweather;
+
+public class OpenweatherClient {
+
+	public static void main(String[] args) {
+		System.out.println(">>> Welcom to Ning's weather service <<<");
+		System.out.println("Please type the city you want to search and hit Enter, e.g. Oulu,fi");
+		
+		
+	}
+}
