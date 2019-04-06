5e06c4445d8cdfc00e73aaeb3423177399cd2f4c
Ning Dai <daining1298@hotmail.com>
2019-04-06 08:01:38 +0300

    fix client ClassNotFound Exception

--- implementation/client/src/main/java/com/ning/openweather/OpenweatherClient.java
+++ implementation/client/src/main/java/com/ning/openweather/OpenweatherClient.java
@@ -9,16 +9,19 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 
 public class OpenweatherClient {
-	final static String OPEN_WEATHER_URL = "http://localhost:8080?weather?location=";
+	final static String OPEN_WEATHER_URL = "http://localhost:8080/weather?location=%s";
 
 	public static void main(String[] args) throws IOException {
 		System.out.println(">>> Welcom to Ning's weather service <<<");
 		System.out.println("Please type the city you want to search and hit Enter, e.g. Oulu,fi");
 		System.out.println("Or just hit Enter, by default it will fetch Oulu's temperature");
 		
-		String location;
+		String location="Oulu,fi";
 		Console console = System.console();
-        location = console.readLine("Please enter location : ");  
+		String input  = console.readLine("Please enter location : ");  
+		if(!input.isEmpty()) {
+			location = input;
+		}
 		URL url = new URL(String.format(OPEN_WEATHER_URL, location));
         JsonFactory factory = new JsonFactory();
         JsonParser parser;
@@ -31,7 +34,7 @@ public class OpenweatherClient {
 	 
             if (JsonToken.FIELD_NAME.equals(token) && "currentTemperature".equals(parser.getCurrentName())) {
             	token = parser.nextToken();
-            	System.out.println(location + "'s current temperature is "+parser.getText());
+            	System.out.println(location + "'s current temperature is "+parser.getText() + " degree celsius.");
             }
         }
 	}
